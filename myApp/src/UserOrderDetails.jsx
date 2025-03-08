cd095f8842930d30df969676b951ebdcb3c705c6
Samragi Dhakal <samragyeedhakal883@gmail.com>
2025-03-08 21:00:00 +0545

    Added User Order Details Interface

---
+++ myApp/src/UserOrderDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Order Details</h1>
+        <h2>History Timeline</h2>
+      </header>
+
+      <div className="content">
+        <p>This user has booked you on date () for this purpose.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
